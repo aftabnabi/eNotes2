d468424c79743b7bfa8d12a9c8a54ade55151877
aftabnabi <aftab.pucit@gmail.com>
2020-11-24 21:39:07 +0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  name = "Angular";
+  obj: HTMLElement;
+  public constructor() {}
+
+  menu(): void {
+    this.obj = document.getElementById("nav") as HTMLElement;
+
+    if (obj.style.display == "none") {
+      obj.style.display = block;
+    } else {
+      obj.style.display = none;
+    }
+  }
 }
